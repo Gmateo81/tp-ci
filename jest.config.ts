f3dc77a37c43c8d751673134334afe92ac7f0161
mateo.gayrard <mateo.gayrard@etu.umontpellier.fr>
2026-04-22 15:16:01 +0200

    add globalSetup

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ export default {
   testMatch: ["**/tests/**/*.test.ts"],
   setupFilesAfterEnv: ["./tests/setup.ts"],
   forceExit: true,
+  globalSetup: "./tests/globalSetup.ts",
   modulePaths: ["<rootDir>/backend/node_modules"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "./tsconfig.json" }],
--- tests/setup.ts
+++ tests/setup.ts
@@ -10,21 +10,6 @@ export const testPool = new Pool({
   database: process.env.CITY_API_DB_NAME,
 });
 
-// Créer la table si elle n'existe pas
-beforeAll(async () => {
-  await testPool.query(`
-    CREATE TABLE IF NOT EXISTS city (
-      id              SERIAL PRIMARY KEY,
-      department_code VARCHAR(10)  NOT NULL,
-      insee_code      VARCHAR(10),
-      zip_code        VARCHAR(10),
-      name            VARCHAR(255) NOT NULL,
-      lat             FLOAT        NOT NULL,
-      lon             FLOAT        NOT NULL
-    )
-  `);
-});
-
 // Vider la table avant chaque test pour garantir l'isolation entre les cas.
 beforeEach(async () => {
   await testPool.query("TRUNCATE TABLE city RESTART IDENTITY CASCADE");
