01d51ee25f287d84c88b48f11ee92bcdcd84d3d1
Charley Bignon <charley.bignon@etu.umontpellier.fr>
2026-04-28 18:52:09 +0200

    test: correct bug

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -21,7 +21,7 @@ for (const envVar of requiredEnvVars) {
 }
 
 const ADDR = process.env.CITY_API_ADDR || "127.0.0.1";
-const PORT = parseInt(process.env.CITY_API_PORT || "0", 10);
+const PORT = parseInt(process.env.CITY_API_PORT || "2022", 10);
 
 export const pool = new Pool({
   host: process.env.CITY_API_DB_URL,
